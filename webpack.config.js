9951dd6b0da4a742858f1befdc373581027bea81
Randy Halim <randy.halim@outlook.com>
2020-08-01 10:01:57 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+
+/** @type {webpack.Configuration} */
+const config = {
+	entry: './index.ts',
+	module: {
+		rules: [{
+			test: /\.ts?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/,
+		}],
+	},
+	resolve: { extensions: ['.tsx', '.ts', '.js'] },
+	output: { path: path.resolve(__dirname, 'dist') },
+	mode: 'development'
+};
